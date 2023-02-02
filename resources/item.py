119229c6c7683f5d1ef341056fc1202cf44dae9f
Carlos Garcia <qesloqpongo@gmail.com>
2023-02-02 14:19:27 -0500

    Initial commit of the creation of API with Flask tutorial

---
+++ resources/item.py
@@ -0,0 +1,67 @@
+from flask_jwt import jwt_required
+from flask_restful import Resource, reqparse
+from models.item import ItemModel
+
+class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+                        type = float,
+                        required = True,
+                        help = "This field cannot be left blank")
+    
+    parser.add_argument('store_id',
+                    type = int,
+                    required = True,
+                    help = "Every item needs a store id")
+
+    @jwt_required()
+    def get(self, name):
+        item = ItemModel.find_by_name(name)
+        if item:
+            return item.json()
+
+        return{"message":"Item not found"}, 404
+
+    def post(self, name):
+        if ItemModel.find_by_name(name):
+            return {'message': 'An item with name {} already exists'.format(name)}, 400 #400 bad request
+        
+        data = Item.parser.parse_args()
+    
+        item = ItemModel(name, **data)
+        
+
+        # try:
+        item.save_to_db()
+
+        # except:
+
+        #     return {"message":"An error ocurred inserting the item"}, 500
+            
+        return item.json(), 201  # 201 http status code for created resource
+
+    def delete(self, name):
+        item = ItemModel.find_by_name(name)
+        
+        if item:
+            item.delete_from_db()
+        
+        return {'message': 'Item deleted'}
+
+    def put(self, name):
+        data = Item.parser.parse_args()
+        item = ItemModel.find_by_name(name)
+        
+        if item is None:
+            item = ItemModel(name, **data)  #**data passes the data atributtes
+        else:
+            item.price = data['price']
+            
+        item.save_to_db()    
+            
+        return item.json()
+
+
+class ItemList(Resource):
+    def get(self):
+        return {'items': list(map(lambda x: x.json(), ItemModel.query.all()))}
